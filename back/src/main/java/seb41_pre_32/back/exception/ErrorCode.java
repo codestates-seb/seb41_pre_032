2ae62a42040131c7a3e4885162efaae660c2afc8
sojeonglee <sojeonglee0125@gmail.com>
2022-12-21 15:36:19 +0900

    feat : 커스텀 예외 클래스 + 예외 코드 정리 클래스 생성

---
+++ back/src/main/java/seb41_pre_32/back/exception/ErrorCode.java
@@ -0,0 +1,26 @@
+package seb41_pre_32.back.exception;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum ErrorCode {
+
+    // USER
+    DUPLICATED_USER_ID("1001"),
+    DUPLICATED_USER_EMAIL("1002");
+
+    //QUESTION
+
+
+    //ANSWER
+
+    private final String code;
+
+    ErrorCode(String code) {
+        this.code = code;
+    }
+
+    @JsonValue
+    public String getCode() {
+        return code;
+    }
+}
