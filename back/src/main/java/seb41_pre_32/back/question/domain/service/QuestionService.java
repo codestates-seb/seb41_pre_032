3f671053a0f183e1db9efbe34d020f1217bde2f9
sojeonglee <sojeonglee0125@gmail.com>
2022-12-23 07:26:15 +0900

    refactor : Qeuestion 생성 시 user 도메인 같이 입력되도록 수정, 조회 페이징 처리 로직 추가

--- back/src/main/java/seb41_pre_32/back/question/domain/service/QuestionService.java
+++ back/src/main/java/seb41_pre_32/back/question/domain/service/QuestionService.java
@@ -1,22 +1,30 @@
 package seb41_pre_32.back.question.domain.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import seb41_pre_32.back.exception.question.QuestionNotFoundException;
+import seb41_pre_32.back.question.domain.dto.QuestionPostDto;
 import seb41_pre_32.back.question.domain.entity.Question;
 import seb41_pre_32.back.question.domain.repository.QuestionRepository;
+import seb41_pre_32.back.user.domain.User;
+import seb41_pre_32.back.user.service.UserService;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class QuestionService {
+
     private final QuestionRepository questionRepository;
+    private final UserService userService;
 
-    public QuestionService(QuestionRepository questionRepository) {
-        this.questionRepository = questionRepository;
-    }
-
-    public Question createQuestion(Question question) {
-
+    public Question createQuestion(QuestionPostDto questionPostDto) {
+        User user = userService.findUser(Long.parseLong(questionPostDto.getUserId()));
+        Question question = questionPostDto.toQuestion();
+        question.addUser(user);
         return questionRepository.save(question);
     }
 
@@ -37,8 +45,8 @@ public class QuestionService {
         return findVerifiedQuestion(questionId);
     }
 
-    public List<Question> findQuestions() {
-        return (List<Question>) questionRepository.findAll();
+    public Page<Question> findQuestions(int page, int size) {
+        return questionRepository.findAll(PageRequest.of(page, size, Sort.by("modifiedDate")));
     }
 
     public void deleteQuestion(Long questionId) {
@@ -47,10 +55,7 @@ public class QuestionService {
     }
 
     public Question findVerifiedQuestion(Long questionId) {
-        Optional<Question> optionalQuestion =
-                questionRepository.findById(questionId);
-        Question findQuestion =
-                optionalQuestion.orElseThrow();
-        return findQuestion;
+        return questionRepository.findById(questionId)
+                .orElseThrow(() -> new QuestionNotFoundException());
     }
 }
