12fcc485a04bfb39272220011b5fe1f2b792c89f
sojeonglee <sojeonglee0125@gmail.com>
2022-12-23 07:24:26 +0900

    feat : 질문 조회 시 연관관계 유저 동시 조회하도록 수정

--- back/src/main/java/seb41_pre_32/back/question/domain/repository/QuestionRepository.java
+++ back/src/main/java/seb41_pre_32/back/question/domain/repository/QuestionRepository.java
@@ -1,8 +1,20 @@
 package seb41_pre_32.back.question.domain.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import seb41_pre_32.back.question.domain.entity.Question;
 
-public interface QuestionRepository extends JpaRepository<Question, Long> {
+import java.util.Optional;
 
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    @Override
+    @EntityGraph(attributePaths = {"user"})
+    Page<Question> findAll(Pageable pageable);
+
+    @Override
+    @Query("select q from Question q left join fetch q.user")
+    Optional<Question> findById(Long id);
 }
