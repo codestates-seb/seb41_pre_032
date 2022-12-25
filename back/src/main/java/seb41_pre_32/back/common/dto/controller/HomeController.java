e60fc7bb24a2191d072a14c2305bd6a8057f6fd2
sojeonglee <sojeonglee0125@gmail.com>
2022-12-25 16:28:37 +0900

    feat : 메인 화면 추가

---
+++ back/src/main/java/seb41_pre_32/back/common/dto/controller/HomeController.java
@@ -0,0 +1,13 @@
+package seb41_pre_32.back.common.dto.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "서버 정상 작동 확인";
+    }
+}
