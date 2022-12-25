b69969bebcfddac9f8516e0bd2c7fc874b8c9d9d
sojeonglee <sojeonglee0125@gmail.com>
2022-12-25 15:20:15 +0900

    test : 배포 테스트 -16

--- back/scripts/deploy.sh
+++ back/scripts/deploy.sh
@@ -2,7 +2,7 @@ REPOSITORY=/home/ec2-user/actions
 PROJECT_NAME=back
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f $PROJECT_NAME)
@@ -22,6 +22,7 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR_NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
+cd $REPOSITORY
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
