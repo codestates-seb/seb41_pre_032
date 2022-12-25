016cec187db65a4538815dfdf41307c42c3b5a65
sojeonglee <sojeonglee0125@gmail.com>
2022-12-25 16:07:09 +0900

    test : 배포 테스트 -21 (될대로 되라~)

---
+++ back/scripts/deploy.sh
@@ -0,0 +1,28 @@
+REPOSITORY=/home/ec2-user/actions
+PROJECT_NAME=backend
+
+echo "> Build 파일 복사"
+cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -f $PROJECT_NAME)
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다"
+else
+  echo "> kill -9 $CURRENT_PID"
+  kill -9 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR_NAME: $JAR_NAME"
+echo "> $JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+nohup java -jar $JAR_NAME > /dev/null 2> /dev/null < /dev/null &
