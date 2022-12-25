95f6aba6ae1506fa13387639e6c5f9ac9686eca6
sojeonglee <sojeonglee0125@gmail.com>
2022-12-25 14:53:35 +0900

    test : 배포 테스트 -14-2

--- back/scripts/deploy.sh
+++ back/scripts/deploy.sh
@@ -1,5 +1,8 @@
 REPOSITORY=/home/ec2-user/actions
-PROJECT_NAME=back-0.0.1-SNAPSHOT.jar
+PROJECT_NAME=back
+
+echo "> Build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f $PROJECT_NAME)
@@ -15,11 +18,12 @@ else
 fi
 
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/build/libs/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR_NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
+echo "> $JAR_NAME 실행"
 echo "> $JAR_NAME 실행"
 nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
