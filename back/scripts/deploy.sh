da97f8f19b95b89722f8d9574f607cdd7ec0afdd
sojeonglee <sojeonglee0125@gmail.com>
2022-12-25 15:41:05 +0900

    test : 배포 테스트 -19

--- back/scripts/deploy.sh
+++ back/scripts/deploy.sh
@@ -1,8 +1,8 @@
 REPOSITORY=/home/ec2-user/actions
-PROJECT_NAME=back
+PROJECT_NAME=backend
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/deploy/*.jar $REPOSITORY/
+cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 CURRENT_PID=$(pgrep -f $PROJECT_NAME)
