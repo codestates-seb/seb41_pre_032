a76ef1507212ec1dc6932539ce5a47b10d05d547
sojeonglee <sojeonglee0125@gmail.com>
2022-12-26 09:09:27 +0900

    feat : cors 매핑 전체 허용 추가

---
+++ back/src/main/java/seb41_pre_32/back/config/WebConfig.java
@@ -0,0 +1,17 @@
+package seb41_pre_32.back.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(false);
+    }
+}
