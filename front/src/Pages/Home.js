497f09baae3d7bf16483c1487ad4c142c429fec3
Valentin1495 <newface95@icloud.com>
2022-12-27 21:02:48 +0900

    feat: pagination

--- front/src/Pages/Home.js
+++ front/src/Pages/Home.js
@@ -5,20 +5,27 @@ import useFetch from '../util/useFetch';
 
 // 새로운 페이지에 아래 스타일 컴포넌트를 최상단에 깔아줘야함
 const HomeWrap = styled.div`
-  width:100%;
-  max-width:1264px;
-  margin:0 auto ;
-  display:flex;
-  justify-content:space-between;
-  text-align:left;
-`
+  width: 100%;
+  max-width: 1264px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  text-align: left;
+`;
 function Home() {
-  const [data, isPending, error] = useFetch(`${process.env.REACT_APP_API_URL}/api/questions?page=1&size=100`);
+  const [data, isPending, error] = useFetch(
+    `${process.env.REACT_APP_API_URL}/api/questions?page=1&size=100`
+  );
 
   return (
     <HomeWrap>
       <Sidebar />
-      <QuestionList title="Top Questions"data={data} isPending={isPending} error={error}/>
+      <QuestionList
+        title='Top Questions'
+        data={data}
+        isPending={isPending}
+        error={error}
+      />
     </HomeWrap>
   );
 }
--- front/src/Pages/SignUp.js
+++ front/src/Pages/SignUp.js
@@ -1,16 +1,16 @@
-import SignIntro from "../Components/SignupIntro";
-import SignupInfo from "../Components/SignInfo";
-import styled from "styled-components";
+import SignupInfo from '../Components/SignupInfo';
+import SignIntro from '../Components/SignupIntro';
+import styled from 'styled-components';
 
 const SignupWrap = styled.div`
   background-color: hsl(210, 8%, 95%);
-  >.signUpContainer {
-    width:100%;
-    max-width:126.4rem;
+  > .signUpContainer {
+    width: 100%;
+    max-width: 126.4rem;
     height: calc(100vh - 50px);
-    margin:0 auto;
-    padding:2.4rem;
-    display:flex;
+    margin: 0 auto;
+    padding: 2.4rem;
+    display: flex;
     justify-content: center;
     align-items: center;
   }
@@ -19,7 +19,7 @@ const SignupWrap = styled.div`
 const SignUp = () => {
   return (
     <SignupWrap>
-      <div className="signUpContainer">
+      <div className='signUpContainer'>
         <SignIntro />
         <SignupInfo />
       </div>
