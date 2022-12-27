3bad2e2051bc7c3f9c62f2c2bb654a83e648dfb7
sojeonglee <sojeonglee0125@gmail.com>
2022-12-28 08:46:36 +0900

    refactor : AnswerController 리팩토링 완료.

--- back/src/main/java/seb41_pre_32/back/answer/controller/AnswerController.java
+++ back/src/main/java/seb41_pre_32/back/answer/controller/AnswerController.java
@@ -14,41 +14,37 @@ import seb41_pre_32.back.auth.utils.LoginUser;
 
 import javax.validation.Valid;
 
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/answers")
 public class AnswerController {
+
     private final AnswerService answerService;
 
     // Answer 등록
     @PostMapping
     public ResponseEntity postAnswer(@RequestBody @Valid final AnswerPostDto answerPostDto,
-                                     @LoginUser AuthInfo authInfo) {
-
-        Answer createdAnswer = answerService.createAnswer(answerPostDto, authInfo);
+                                     @LoginUser final AuthInfo authInfo) {
         return new ResponseEntity<>(
-                AnswerResponseDto.of(createdAnswer),
+                AnswerResponseDto.of(answerService.createAnswer(answerPostDto, authInfo)),
                 HttpStatus.CREATED);
     }
 
     // Answer 수정
     @PatchMapping("/{answerId}")
     public ResponseEntity patchAnswer(@PathVariable("answerId") final Long answerId,
-                                      @LoginUser AuthInfo authInfo,
-                                      @RequestBody @Valid AnswerPatchDto answerPatchDto) {
-
-        Answer updatedAnswer = answerService.updateAnswer(answerId, authInfo, answerPatchDto.getContents());
-        return new ResponseEntity<>(AnswerResponseDto.of(updatedAnswer),
+                                      @LoginUser final AuthInfo authInfo,
+                                      @RequestBody @Valid final AnswerPatchDto answerPatchDto) {
+        return new ResponseEntity<>(
+                AnswerResponseDto.of(answerService.updateAnswer(answerId, authInfo, answerPatchDto)),
                 HttpStatus.OK);
     }
 
-    //answer 삭제
+    //Answer 삭제
     @DeleteMapping("/{answerId}")
     public ResponseEntity deleteAnswer(@PathVariable("answerId") final Long answerId,
                                        @LoginUser AuthInfo authInfo) {
         answerService.deleteAnswer(answerId, authInfo);
-
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
