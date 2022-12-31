6aa6f415821eaaae76832468559548814eaf3c8a
sojeonglee <sojeonglee0125@gmail.com>
2022-12-31 16:13:18 +0900

    feat : 토큰 관련 에러코드 수정

--- back/src/main/java/seb41_pre_32/back/exception/auth/ExtractTokenFailedException.java
+++ back/src/main/java/seb41_pre_32/back/exception/auth/ExtractTokenFailedException.java
@@ -6,6 +6,6 @@ import seb41_pre_32.back.exception.ErrorLogicException;
 public class ExtractTokenFailedException extends ErrorLogicException {
 
     public ExtractTokenFailedException() {
-        super(ErrorCode.NOT_VALID_TOKEN, "엑세스 토큰이 잘못되었습니다.");
+        super(ErrorCode.NOT_VALID_TOKEN, "토큰 값이 없거나 잘못되었습니다.");
     }
 }
