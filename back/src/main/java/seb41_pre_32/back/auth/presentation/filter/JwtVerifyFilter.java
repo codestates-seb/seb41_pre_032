efaa81daf9aa025538aecd87cf81aac817af75b1
sojeonglee <sojeonglee0125@gmail.com>
2023-01-01 22:07:22 +0900

    fix: 권한 설정 securityContext 지운 코드 수정 완료.

--- back/src/main/java/seb41_pre_32/back/auth/presentation/filter/JwtVerifyFilter.java
+++ back/src/main/java/seb41_pre_32/back/auth/presentation/filter/JwtVerifyFilter.java
@@ -3,8 +3,13 @@ package seb41_pre_32.back.auth.presentation.filter;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.security.SignatureException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import seb41_pre_32.back.auth.utils.CustomAuthorityUtils;
 import seb41_pre_32.back.auth.utils.JwtTokenizer;
 
 import javax.servlet.FilterChain;
@@ -12,12 +17,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
 public class JwtVerifyFilter extends OncePerRequestFilter {
 
     private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
 
     @Override
     protected void doFilterInternal(final HttpServletRequest request,
@@ -25,6 +32,7 @@ public class JwtVerifyFilter extends OncePerRequestFilter {
                                     final FilterChain filterChain) throws ServletException, IOException {
         try {
             Map<String, Object> claims = verifyJws(request);
+            setAuthenticationToContext(claims);
         } catch (SignatureException se) {
             request.setAttribute("exception", se);
         } catch (ExpiredJwtException je) {
@@ -45,5 +53,12 @@ public class JwtVerifyFilter extends OncePerRequestFilter {
     private Map<String, Object> verifyJws(final HttpServletRequest request) {
         String jws = request.getHeader("Authorization").replace("Bearer ", "");
         return jwtTokenizer.getClaims(jws).getBody();
+    }
+
+    private void setAuthenticationToContext(Map<String, Object> claims) {
+        String username = (String) claims.get("username");
+        List<GrantedAuthority> authorities = authorityUtils.createAuthorities((String) claims.get("role"));
+        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
